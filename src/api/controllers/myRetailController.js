1149be5bd0e0591dfe099d2c62d76fcb836e0fa7
Bob Laskowski <blaskowski94@gmail.com>
2019-03-17 21:15:32 -0700

    feat: error handling & async/await on controller

--- src/api/controllers/myRetailController.js
+++ src/api/controllers/myRetailController.js
@@ -1,57 +1,75 @@
 const mongoose = require('mongoose')
 const fetch = require('node-fetch')
+const _ = require('lodash')
 
 Product = mongoose.model('Product')
+apiUrl = productId => `https://redsky.target.com/v2/pdp/tcin/${productId}?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics,esp,deep_red_labels,available_to_promise_network`
 
-const listProducts = (req, res) => {
-    Product.find({}, (err, product) => {
-        if (err) res.send(err)
-        console.log('GET successful')
-        res.json(product)
-    })
+// TODO: params validation, error handling, valid http response codes
+
+const listProducts = async (req, res) => {
+    try {
+        const list = await Product.find({})
+        console.log('GET successful: list')
+        res.json(list)
+    } catch (error) {
+        console.error(error)
+        res.send(error)
+    }
 }
 
 const createProduct = async (req, res) => {
-    const newProduct = new Product(req.body)
-    await newProduct.save((err, product) => {
-        if (err) res.send(err)
+    try {
+        const newProduct = new Product(req.body)
+        const product = await newProduct.save()
         console.log(`POST successful: ${product.productId}`)
         res.json(product)
-    })
+    } catch (error) {
+        console.error(error)
+        res.send(error)
+    }
 }
 
 const getProduct = async (req, res) => {
-    const productId = req.params.id
-    const response = await (await fetch(`https://redsky.target.com/v2/pdp/tcin/${productId}`)).json()
-    //console.log(response)
-    Product.findOne({productId: req.params.id}, (err, product) => {
-        if (err) res.send(err)
-        console.log(`GET successful: ${product.productId}`)
-        res.json(product)
-    })
+    try {
+        const productId = req.params.id
+        const apiProduct = await (await fetch(apiUrl(productId))).json()
+        const productName = _.get(apiProduct, 'product.item.product_description.title', 'Unavailable')
+
+        const dbProduct = await Product.findOne({productId})
+        const productPrice = _.get(dbProduct, 'price', 'Unavailable')
+
+        res.json({productId, productName, productPrice})
+    } catch (error) {
+        console.error(error)
+        res.send(error)
+    }
 }
 
-const updateProduct = (req, res) => {
-    Product.findOneAndUpdate({productId: req.params.id}, req.body, {
-        new: true, runValidators: true
-    }, (err, product) => {
-        if (err) res.send(err)
-        console.log(`PUT successful: ${product.productId}`)
-        res.json(product)
-    })
+const updateProduct = async (req, res) => {
+    try {
+        const productId = req.params.id
+        let response = await Product.findOneAndUpdate({productId}, req.body, {
+            new: true, runValidators: true
+        })
+        console.log(`Update successful: ${productId}`)
+        res.json(response)
+    } catch (error) {
+        console.error(error)
+        res.send(error)
+    }
 }
 
 const deleteProduct = async (req, res) => {
+    const productId = req.params.id
     await Product.deleteOne({
-        productId: req.params.id
+        productId
     }, (err) => {
         if (err) res.send(err)
-        const message = 'DELETE successful'
+        const message = `DELETE successful: ${productId}`
         console.log(message)
         res.json({message})
     })
 }
 
 module.exports = {listProducts, createProduct, getProduct, deleteProduct, updateProduct}
-
-
--- src/server.js
+++ src/server.js
@@ -1,37 +1,29 @@
 const express = require('express')
-app = express()
-port = process.env.PORT || 3000
-mongoose = require('mongoose')
-Product = require('./api/models/myRetailModel') //created model loading here
-bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const Product = require('./api/models/myRetailModel')
+const routes = require('./api/routes/myRetailRoutes')
 
-// mongoose instance connection url connection
-mongoose.connect('mongodb://localhost/myRetailDb', {useNewUrlParser: true, useCreateIndex: true})
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', async () => {
-    try {
-        const pro = new Product({productId: 50000000, price: '$5.25'})
-        await pro.save()
-    } catch (ex) {}
-    app.emit('ready')
-})
+const port = process.env.PORT || 3000
+const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-
-const routes = require('./api/routes/myRetailRoutes') //importing route
-routes(app) //register the route
-
-app.on('ready', function() {
-    app.listen(3000, function() {
-        console.log('app is ready')
+routes(app) 
+app.on('ready', () => {
+    app.listen(port, () => {
+        console.log(`myRetail RESTful service is ready, listening on port ${port}`)
     })
 })
-
-console.log('myRetail RESTful API server started on: ' + port)
-
-app.use(function(req, res) {
+app.use((req, res) => {
     res.status(404).send({url: req.originalUrl + ' not found on server'})
 })
 
+// connect to mongodb
+mongoose.connect('mongodb://localhost/myRetailDb', {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false})
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+    // TODO: load default data here
+    app.emit('ready')
+})
